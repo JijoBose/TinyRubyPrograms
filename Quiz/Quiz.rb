5e91f926b3e243352a975c1bb1f45666fe3976c0
JijoBose <bosejijo@gmail.com>
2016-11-30 20:31:33 +0530

    Quiz program

---
+++ Quiz/Quiz.rb
@@ -0,0 +1,45 @@
+################################################################################
+#  TinyRubyPrograms is a collection of tiny programs written in Ruby.
+#  Copyright (C) 2016  Jijo Bose
+#
+#  TinyRubyPrograms is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  TinyRubyPrograms is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with TinyRubyPrograms.  If not, see <http://www.gnu.org/licenses/>
+################################################################################
+
+class Quiz
+    def questions
+      quest = ['First question','Second question']
+      options = [["1: answer1","2: answer2"],["1: answer1","2: answer2"]]
+      answ = [1,2]
+      inc = 0
+
+      answ.each do
+        puts "#{quest[inc]}"
+        op1 = 0
+        op2 = 1
+        puts "Your Options:\n #{options[op1][op1]}\n #{options[op1][op2]}"
+        usrinc = gets.to_i
+        if answ[inc] == usrinc
+          puts "Correct"
+          puts ""
+        else
+          puts "incorrect\n"
+          puts ""
+        end
+        inc += 1
+      end
+    end
+end
+
+audience = Quiz.new
+audience.questions
