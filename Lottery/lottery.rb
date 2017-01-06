0c2290278ba856c9f1e1e24c8ab61b980577626a
JijoBose <bosejijo@gmail.com>
2017-01-06 11:44:39 +0530

    improved code

--- Lottery/lottery.rb
+++ Lottery/lottery.rb
@@ -2,6 +2,19 @@ class LotteryMachine
   def luckydraw(ticketno)
     userin = ticketno
     lottno = "#{rand(0...10)}#{rand(0...10)}#{rand(0...10)}#{rand(0...10)}#{rand(0...10)}#{rand(0...10)}"
+    userin.each do
+      if userin == lottno
+        print "You won the Jackpot\n"
+        print "Your number: #{userin}\n"
+        print "winnin number: #{lottno}\n"
+      else
+        puts "Better try next time\n"
+        print "Your number:    #{userin}\n"
+        print "winning number: #{lottno}\n"
+      end
+    end
+
+=begin
     if userin == lottno
       print "You won the Jackpot\n"
       print "Your number: #{userin}\n"
@@ -11,10 +24,11 @@ class LotteryMachine
       print "Your number:    #{userin}\n"
       print "winning number: #{lottno}\n"
     end
+=end
   end
 end
 
 print "enter any 6 digits: "
-userin = gets.to_i
+userin = ['789845','456512','326598','546578']
 lottery = LotteryMachine.new
 lottery.luckydraw(userin)
--- RandomQuote/randomquote.rb
+++ RandomQuote/randomquote.rb
@@ -1,3 +1,8 @@
+def quotemachine(inquotes)
+  puts inquotes[rand(0..3)]
+end
+
 quotes = ["Live life as if it's your last","Ruby a day keeps problems away",
           "More Linux, more freedom","More opensource, more improvement"]
-puts quotes[rand(0..3)]
+
+quotemachine(quotes)
